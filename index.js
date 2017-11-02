aff29022f42cdea9db0da8eb208e2c50ce276590
JortvD <jortvandriel@hotmail.com>
2017-11-02 15:36:39 +0100

    Added the package.json and index file

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = function() {
+	var self = {};
+
+	var package = require("../package.json");
+	
+	self.name = package["name"];
+	self.version = package["version"];
+
+	self.promise = require("./lib/promise");
+
+	return self;
+}
